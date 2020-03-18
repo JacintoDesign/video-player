6ba6dadcf4fece487e6fc3829c0a506e83425081
JACINTO WONG <jacinto.wong@cbc.ca>
2020-03-17 22:57:48 -0400

    volume bar & mute  functionality

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ const fullscreen = player.querySelector('.fullscreen');
 // Global Variables
 
 
-// Play & Pause Functionality ---------------------- //
+// Play & Pause ----------------------------------- //
+
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 function togglePlay() {
@@ -29,13 +30,7 @@ function togglePlay() {
 // on video end, show play button icon
 video.addEventListener('ended', () => playBtn.innerHTML = '<i class="fas fa-play" title="Play"></i>');
 
-// Format current time, duration
-function displayTime(time) {
-    var minutes = Math.floor((time % 3600) / 60);
-    var seconds = Math.floor(time % 60);
-    seconds = seconds > 9? seconds: `0${seconds}`;
-    return `${minutes}:${seconds}`;
-}
+// Progress Bar ---------------------------------- //
 
 // Update progress bar as video plays
 video.addEventListener('timeupdate', updateProgress);
@@ -46,6 +41,14 @@ function updateProgress(e) {
     duration.innerHTML = `${displayTime(video.duration)}`
 }
 
+// Format current time, duration 
+function displayTime(time) {
+    var minutes = Math.floor((time % 3600) / 60);
+    var seconds = Math.floor(time % 60);
+    seconds = seconds > 9? seconds: `0${seconds}`;
+    return `${minutes}:${seconds}`;
+}
+
 // Click to seek within the video
 progressRange.addEventListener('click', setProgress)
 function setProgress(e) {
@@ -54,9 +57,43 @@ function setProgress(e) {
     video.currentTime = newTime * video.duration;
 }
 
+// Volume Controls --------------------------- //
 
+let lastVolume = 1;
 
+// Mute
+volumeIcon.addEventListener('click', toggleMute);
+function toggleMute() {
+    if (video.volume) {
+        lastVolume = video.volume;
+        video.volume = 0;
+        volumeIcon.innerHTML = '<i class="fas fa-volume-mute" title="Unmute"></i>';
+        volumeBar.style.width = 0;    
+    } else {
+        video.volume = lastVolume;
+        volumeIcon.innerHTML = '<i class="fas fa-volume-up" title="Mute"></i>';
+        volumeBar.style.width = `${lastVolume * 100}%`;  
+    }
+}
 
+// Volume Bar
+volumeRange.addEventListener('click', changeVolume);
+function changeVolume(e) {
+    let volume = e.offsetX / volumeRange.offsetWidth;
+    if (volume < 0.1) { volume = 0; }
+    if (volume > 0.9) { volume = 1; }
+    volumeBar.style.width = `${volume * 100}%`
+    video.volume = volume;
+    if (volume > 0.7) {
+        volumeIcon.innerHTML = '<i class="fas fa-volume-up"></i>';
+    } else if (volume < 0.7 && volume > 0) {
+        volumeIcon.innerHTML = '<i class="fas fa-volume-down"></i>';
+    } else if (volume == 0) {
+        volumeIcon.innerHTML = '<i class="fas fa-volume-off"></i>';
+    }
+    lastVolume = volume;
+    console.log(volume);
+}
 
 
 
