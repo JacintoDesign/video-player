5568e9a3017c3ab2bb211cb01c3ccbc7bbd73b03
JACINTO WONG <jacinto.wong@cbc.ca>
2020-03-17 21:13:04 -0400

    show/hide controls, const setup

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('eat my shorts');
+const player = document.querySelector('.player');
+const video = player.querySelector('.video');
+const progressRange = player.querySelector('.progress-range');
+const progressBar = player.querySelector('.progress-bar');
+const playBtn = player.querySelector('.play-controls');
+const volumeIcon = player.querySelector('.volume-icon');
+const volumeRange = player.querySelector('.volume-range');
+const volumeBar = player.querySelector('.volume-bar');
+const speed = player.querySelector('.player-speed');
+const currentTime = player.querySelector('.time-elapsed');
+const duration = player.querySelector('.time-duration');
+const fullscreen = player.querySelector('.fullscreen');
+
+
+// playBtn.innerHTML = '<i class="fas fa-pause"></i>'
