626ccad53677891c42450d4602ad93a0f50dbc85
JACINTO WONG <jacinto.wong@cbc.ca>
2020-03-17 22:33:36 -0400

    adjusted for larger video, progress bar functionality

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const fullscreen = player.querySelector('.fullscreen');
 // Global Variables
 
 
-// Play / Pause Functionality
+// Play & Pause Functionality ---------------------- //
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 function togglePlay() {
@@ -26,10 +26,33 @@ function togglePlay() {
         playBtn.innerHTML = '<i class="fas fa-play" title="Play"></i>'
     }
 }
+// on video end, show play button icon
+video.addEventListener('ended', () => playBtn.innerHTML = '<i class="fas fa-play" title="Play"></i>');
 
+// Format current time, duration
+function displayTime(time) {
+    var minutes = Math.floor((time % 3600) / 60);
+    var seconds = Math.floor(time % 60);
+    seconds = seconds > 9? seconds: `0${seconds}`;
+    return `${minutes}:${seconds}`;
+}
 
+// Update progress bar as video plays
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
+function updateProgress(e) {
+    progressBar.style.width = `${video.currentTime / video.duration * 100}%`;
+    currentTime.innerHTML = `${displayTime(video.currentTime)} /`;
+    duration.innerHTML = `${displayTime(video.duration)}`
+}
 
-
+// Click to seek within the video
+progressRange.addEventListener('click', setProgress)
+function setProgress(e) {
+    const newTime = e.offsetX / progressRange.offsetWidth;
+    progressBar.style.width = `${newTime*100}%`;
+    video.currentTime = newTime * video.duration;
+}
 
 
 
