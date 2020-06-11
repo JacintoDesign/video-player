fb8e9c2d2b602a22911e40e7999d187b7de152e3
JACINTO WONG <jacinto.wong@cbc.ca>
2020-06-11 15:07:48 -0400

    final production code

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const player = document.querySelector('.player');
-const video = player.querySelector('.video');
-const progressRange = player.querySelector('.progress-range');
-const progressBar = player.querySelector('.progress-bar');
+const video = document.querySelector('.video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
 const playBtn = document.getElementById('play-btn');
 const volumeIcon = document.getElementById('volume-icon');
-const volumeRange = player.querySelector('.volume-range');
-const volumeBar = player.querySelector('.volume-bar');
-const speed = player.querySelector('.player-speed');
-const currentTime = player.querySelector('.time-elapsed');
-const duration = player.querySelector('.time-duration');
-const fullscreenBtn = player.querySelector('.fullscreen');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const speed = document.querySelector('.player-speed');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const fullscreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
 
